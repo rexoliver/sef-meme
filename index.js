6a15a2d6ff5c2f1d3a644420f398e4e03e8c1779
Rex Oliver <rexroao@g.clemson.edu>
2020-07-15 15:22:33 -0400

    Add chuck norris api

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let quotesDiv = document.getElementById('quote')
 
 fetch('https://api.chucknorris.io/jokes/random')
+    .then(res => res.json())
+    .then(quote => {
+        quotesDiv.innerHTML += '<p> ${quote.value) </p>'
+    })
