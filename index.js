aa4318ccd637c83a7de69355ef60d5476b5ad2a8
Rex Oliver <rexroao@g.clemson.edu>
2020-07-15 16:41:56 -0400

    api.kanye

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let messageDiv = document.getElementById('quote')
 
-fetch('https://kanye.rest')
+fetch('https://api.kanye.rest')
     .then(res => res.json())
     .then(quote => {
         messageDiv.innerHTML += '<p> ${quote.quote} </p>'
