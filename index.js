efdf424c991b45802db40363717a2f985aea5bea
Rex Oliver <rexroao@g.clemson.edu>
2020-07-15 15:41:30 -0400

    add test

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let quotesDiv = document.getElementById('quote')
 fetch('https://api.chucknorris.io/jokes/random')
     .then(res => res.json())
     .then(quote => {
-        quotesDiv.innerHTML += '<p> ${quote.value) </p>'
+        quotesDiv.innerHTML += '<p> ${quote.value) asdf </p>'
     })
